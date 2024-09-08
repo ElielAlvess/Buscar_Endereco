e58783b7896023d30a8ade59fbf809e874fe17b4
ElielAlvess <eliel_alvess@hotmail.com>
2024-09-08 20:52:57 -0300

    novos ajustes de criacao da base final

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
-
-#Importando bibliotecas.
+# Importando bibliotecas.
 import requests
 import pandas as pd
 import pyautogui as py
+import os
 
 contador = 0
 
-#Lendo arquivo com os CEPS.
-df = pd.read_excel(r'ceps.xlsx')
+# Lendo arquivo com os CEPS.
+df = pd.read_excel(r'C:\Users\eliel\OneDrive\Área de Trabalho\Busca_cep\Buscar_endereco\ceps.xlsx')
 
-#Iterando sobre cada cep da base.
+# Iterando sobre cada cep da base.
 for i in range(len(df)):
     cep = df.loc[i, 'CEP']
 
-    #consultando os ceps pela API e retornandno um JSON com os dados requisitados.
+    # Consultando os CEPs pela API e retornando um JSON com os dados requisitados.
     link = f'https://viacep.com.br/ws/{cep}/json/'
-    requisicao = requests.get(link)
+    requisicao = requests.get(link) 
     requisicao_json = requisicao.json()
 
-    #Tratando erro de dados ausentes dentro do Json requisitado.
+    # Tratando erro de dados ausentes dentro do JSON requisitado.
     try:
-        uf = requisicao_json['uf']
-        cidade = requisicao_json['localidade']
-        logradouro = requisicao_json['logradouro']
-        bairro = requisicao_json['bairro']
-        complemento = logradouro = requisicao_json['complemento']
-
+        uf = requisicao_json.get('uf', '')
+        cidade = requisicao_json.get('localidade', '')
+        logradouro = requisicao_json.get('logradouro', '')
+        bairro = requisicao_json.get('bairro', '')
+        complemento = requisicao_json.get('complemento', '')
     except KeyError:
-        uf = None
-        cidade = None
-        logradouro = None
-        bairro = None
-        complemento = None
+        uf, cidade, logradouro, bairro, complemento = '', '', '', '', ''
 
-    #Criando novas colunas dentro da base de CEP.
+    # Criando novas colunas dentro da base de CEP.
     df.loc[i, 'UF'] = uf
     df.loc[i, 'Cidade'] = cidade
     df.loc[i, 'Logradouro'] = logradouro
     df.loc[i, 'Bairro'] = bairro
     df.loc[i, 'Complemento'] = complemento
 
-#Gera uma base provisoria a cada 10 consultas para caso haja algum erro e nao precisemos recomeçar do zero.
-
+    # Gera uma base provisória a cada 10 consultas para caso haja algum erro e não precisemos recomeçar do zero.
     if contador == 10:
-        endereco = df.to_excel('Enderecos.xlsx')
+        base_provisoria = df.to_excel(r'C:/Users/eliel/OneDrive/Área de Trabalho/Busca_cep/Buscar_endereco/Enderecos_parcial.xlsx', index=False)
         contador = 0
     else:
-        contador = contador + 1
+        contador += 1
 
-#Criando novo arquivo xlsx com o endereco completo
-endreco = df.to_excel('Enderecos.xlsx', index=False)
+# Validando e excluindo arquivo já existente
+caminho_arquivo = r'C:/Users/eliel/OneDrive/Área de Trabalho/Busca_cep/Buscar_endereco/Enderecos.xlsx'
 
-# Mensagem de alerta informando que a base de endereços foi gerada com sucesso!!
+if os.path.exists(caminho_arquivo):
+    os.remove(caminho_arquivo)
+
+# Salvando o arquivo final após a última consulta.
+df.to_excel(caminho_arquivo, index=False)
+os.remove(r'C:\Users\eliel\OneDrive\Área de Trabalho\Busca_cep\Buscar_endereco\Enderecos_parcial.xlsx')
+
+# Mensagem de alerta informando que a base de endereços foi gerada com sucesso!
 py.alert('Base Finalizada!')
-
-
-
